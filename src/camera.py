249ba6400d066ae2eb7646f48e5063ae654fed67
Fatinemourid <162514283+Fatinemourid@users.noreply.github.com>
2025-12-23 02:57:34 +0100

    Changement du fichier camera
    Added comments in French for better understanding.

--- src/camera.py
+++ src/camera.py
@@ -1,20 +1,14 @@
-import cv2
-
-
+import cv2 # Bibliothèque OpenCV
 def open_camera(index: int = 0) -> cv2.VideoCapture:
-    cap = cv2.VideoCapture(index)
+    cap = cv2.VideoCapture(index) # Ouvre la webcam
     if not cap.isOpened():
-        raise RuntimeError("Caméra non accessible")
-    return cap
-
-
+        raise RuntimeError("Caméra non accessible") # Erreur si webcam indisponible
+    return cap  # Retourne la caméra
 def read_frame(cap: cv2.VideoCapture):
-    ret, frame = cap.read()
+    ret, frame = cap.read()  #Lit une image depuis la webcam
     if not ret:
-        raise RuntimeError("Impossible de lire la frame")
-    return frame
-
-
+        raise RuntimeError("Impossible de lire la frame")# Erreur lecture
+    return frame  # Retourne l’image
 def close_camera(cap: cv2.VideoCapture):
-    cap.release()
-    cv2.destroyAllWindows()
+    cap.release() # Libère la webcam
+    cv2.destroyAllWindows() # Ferme les fenêtres
